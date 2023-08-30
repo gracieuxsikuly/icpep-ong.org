60212dcaec39f3378acecd5eea17954efc3f2b86
gracieuxsikuly <graciersikuly@gmail.com>
2023-08-30 12:13:04 +0200

    add all

--- resources/views/livewire/front/contact.blade.php
+++ resources/views/livewire/front/contact.blade.php
@@ -111,7 +111,7 @@
             <div class="map-column clearfix">
                 @php
                     $apiKey = config('maps.api_key');
-                    dd($apiKey);
+                    // dd($apiKey);
                 @endphp
                 <iframe width="100%" height="100%" frameborder="0" style="border:0" referrerpolicy="no-referrer-when-downgrade"
                 src="https://www.google.com/maps/embed/v1/place?key={{ $apiKey }}&zoom=18&q=Oliversoft
