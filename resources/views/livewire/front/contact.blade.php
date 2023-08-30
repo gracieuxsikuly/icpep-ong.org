fb5db2ded1a956524af12aecccf93a75fd44db4d
gracieuxsikuly <graciersikuly@gmail.com>
2023-08-30 09:44:34 +0200

    okqs

--- resources/views/livewire/front/contact.blade.php
+++ resources/views/livewire/front/contact.blade.php
@@ -5,10 +5,10 @@
         <div class="bottom-rotten-curve"></div>
 
         <div class="auto-container">
-            <h1>Contact Us</h1>
+            <h1>Contactez-nous</h1>
             <ul class="bread-crumb clearfix">
-                <li><a href="index.html">Home</a></li>
-                <li class="active">Contact Us</li>
+                <li><a href="/">Acceuil</a></li>
+                <li class="active">Contactez nous</li>
             </ul>
         </div>
 
@@ -22,7 +22,7 @@
             <div class="sec-title centered">
                 <div class="sub-title">Contact Us</div>
                 <h2>Get In Touch</h2>
-                <div class="text">Cupidatat non proident sunt</div>
+                {{-- <div class="text">Cupidatat non proident sunt</div> --}}
             </div>
 
         	<div class="info-boxes">
@@ -32,7 +32,7 @@
                         <div class="inner-box wow fadeInUp" data-wow-delay="0ms">
 							<div class="image-layer lazy-image" data-bg="url('images/resource/contact-image-1.jpg')"></div>
                             <div class="icon-box"><span class="flaticon-home-location-marker"></span></div>
-                            <h4>Our Location</h4>
+                            <h4>Notre addresse</h4>
                             <ul>
                             	<li>16/14 Babor Road, Shyamoly Dhaka.</li>
                             </ul>
@@ -43,7 +43,7 @@
                         <div class="inner-box wow fadeInUp" data-wow-delay="300ms">
                             <div class="image-layer lazy-image" data-bg="url('images/resource/contact-image-2.jpg')"></div>
                             <div class="icon-box"><span class="flaticon-phone-call"></span></div>
-                            <h4>Phone Number</h4>
+                            <h4>Numéro de Télephone</h4>
                             <ul>
                             	<li><a href="tel:(+55)654-545-5418">(+55)  654 - 545 - 5418</a></li>
                                 <li><a href="tel:(+55)654-545-1235">(+55)  654 - 545 - 1235</a></li>
@@ -55,7 +55,7 @@
                         <div class="inner-box wow fadeInUp" data-wow-delay="600ms">
                             <div class="image-layer lazy-image" data-bg="url('images/resource/contact-image-3.jpg')"></div>
                             <div class="icon-box"><span class="flaticon-email"></span></div>
-                            <h4>Email Address</h4>
+                            <h4>Adresse Email</h4>
                             <ul>
                                 <li><a href="mailto:info@example.com">info@example.com</a></li>
                                 <li><a href="mailto:info@loveus.com">info@loveus.com</a></li>
@@ -77,9 +77,9 @@
                 <div class="inner clearfix">
 
                     <div class="sec-title centered">
-                        <div class="sub-title">Inquiry</div>
-                        <h2>Leave a Message</h2>
-                        <div class="text">Cupidatat non proident sunt</div>
+                        {{-- <div class="sub-title">Inquiry</div> --}}
+                        <h2>Laisser un message</h2>
+                        {{-- <div class="text">Cupidatat non proident sunt</div> --}}
                     </div>
 
                     <!-- Contact Form-->
@@ -99,7 +99,7 @@
                                 </div>
 
                                 <div class="col-md-12 col-sm-12 form-group text-center">
-                                    <button class="theme-btn btn-style-one" type="submit" name="submit-form"><span class="btn-title">Send Message</span></button>
+                                    <button class="theme-btn btn-style-one" type="submit" name="submit-form"><span class="btn-title">Evoyer Message</span></button>
                                 </div>
                             </div>
                         </form>
