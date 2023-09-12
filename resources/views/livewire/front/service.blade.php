58bd30d731d03521625f478c07e4d82fde1084f2
gracieuxsikuly <graciersikuly@gmail.com>
2023-09-12 08:29:46 +0200

    super

--- resources/views/livewire/front/service.blade.php
+++ resources/views/livewire/front/service.blade.php
@@ -10,7 +10,7 @@
                         <div class="inner-box">
                             <div class="icon-box"><span class="flaticon-water-bottle"></span></div>
                             {{-- <h3>Pure Water</h3> --}}
-                            <div class="text">
+                            <div class="text" style="text-align: justify;">
                                 L’ICPEP a cultivé un hectare en phase d’essai de la semence du blé et a
                                     obtenu plus de 450 variétés du blé à moins de 6 mois et dispose d’une dizaine des nouvelles
                                      variétés de la pomme de terre en phase d’adaptation à l’INERA, adaptations multi locales et
@@ -24,7 +24,7 @@
                         <div class="inner-box">
                             <div class="icon-box"><span class="flaticon-fruit"></span></div>
                             {{-- <h3>Healthy Food</h3> --}}
-                            <div class="text">ICPEP a accompagné plus de 50 dossiers en justice et obtenu des décisions judiciaires, parmi lesquels des VSBG ;
+                            <div class="text" style="text-align: justify;">ICPEP a accompagné plus de 50 dossiers en justice et obtenu des décisions judiciaires, parmi lesquels des VSBG ;
                                 ICPEP a élaboré plusieurs rapports sur la protection des civils, dénonciations des violations des droits humains-spoliation des biens publics de l’Etat-détournements des deniers publics-dysfonctionnement de l’appareil judiciaire et de l’administration publique… ICPEP a de la même manière mener plusieurs plaidoyers pour relever ces défis et plusieurs résultats se sont avérés satisfaisants ;
                                 </div>
                         </div>
@@ -35,7 +35,7 @@
                         <div class="inner-box">
                             <div class="icon-box"><span class="flaticon-medicine"></span></div>
                             {{-- <h3>Medical Facilities</h3> --}}
-                            <div class="text">ICPEP a rendu public plusieurs rapports de monitoring des lieux de détention, adressés aux autorités judiciaires et exécutives compétentes pour l’amélioration de l’administration de la justice et l’assainissement du milieu carcéral (où loge une population oubliée) ;</div>
+                            <div class="text" style="text-align: justify;">ICPEP a rendu public plusieurs rapports de monitoring des lieux de détention, adressés aux autorités judiciaires et exécutives compétentes pour l’amélioration de l’administration de la justice et l’assainissement du milieu carcéral (où loge une population oubliée) ;</div>
                         </div>
                     </div>
 
@@ -44,7 +44,7 @@
                         <div class="inner-box">
                             <div class="icon-box"><span class="flaticon-reading"></span></div>
                             <h3>Education</h3>
-                            <div class="text">ICPEP a organisé plusieurs formations à l’intention de la population civile et
+                            <div class="text" style="text-align: justify;">ICPEP a organisé plusieurs formations à l’intention de la population civile et
                                 autres bénéficiaires sur les droits humains et leurs mécanismes de protection,
                                 les mécanismes extra-judiciaires ou modes alternatifs de résolution des conflits,
                                  l’art culinaire (avec remise des brevets contresignés par le Ministre Provincial compétent).
@@ -58,7 +58,7 @@
                         <div class="inner-box">
                             <div class="icon-box"><span class="flaticon-adoption"></span></div>
                             {{-- <h3>Adoption</h3> --}}
-                            <div class="text">Grace aux orientations et plaidoyers de ICPEP, plusieurs conflits
+                            <div class="text" style="text-align: justify;">Grace aux orientations et plaidoyers de ICPEP, plusieurs conflits
                                  fonciers et de pouvoir coutumiers ont été clôturés par les instances administratives,
                                   coutumières ou judiciaires compétentes, notamment en territoire de Lubero, Nyiragongo,
                                   Beni, Walikale et Masisi, ville de Goma et de Butembo ;</div>
