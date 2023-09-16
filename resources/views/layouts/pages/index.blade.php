28feb5ae245471e6564a5327365d52c2f5e8562a
gracieuxsikuly <graciersikuly@gmail.com>
2023-09-16 11:57:41 +0200

    good

--- resources/views/layouts/pages/index.blade.php
+++ resources/views/layouts/pages/index.blade.php
@@ -118,7 +118,7 @@
                 	<div class="inner-box">
                     	<div class="icon-box"><span class="flaticon-water-bottle"></span></div>
                         <h3>Pure Water</h3>
-                        <div class="text">Lorem ipsum dolor amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolor magna aliqua enim ad minimveniam quis nostrud exercitation laboris.</div>
+                        <div class="text" style="text-align: justify;">L’ICPEP a cultivé un hectare en phase d’essai de la semence du blé et a obtenu plus de 450 variétés du blé à moins de 6 mois et dispose d’une dizaine des nouvelles variétés de la pomme de terre en phase d’adaptation à l’INERA, adaptations multi locales et petite multiplication dans diverses zones agroécologies de haute terre.</div>
                     </div>
                 </div>
 
@@ -127,16 +127,16 @@
                 	<div class="inner-box">
                     	<div class="icon-box"><span class="flaticon-fruit"></span></div>
                         <h3>Healthy Food</h3>
-                        <div class="text">Lorem ipsum dolor amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolor magna aliqua enim ad minimveniam quis nostrud exercitation laboris.</div>
+                        <div class="text" style="text-align: justify;">ICPEP a accompagné plus de 50 dossiers en justice et obtenu des décisions judiciaires, parmi lesquels des VSBG ; ICPEP a élaboré plusieurs rapports sur la protection des civils, dénonciations des violations des droits humains-spoliation des biens publics de l’Etat-détournements des deniers publics-dysfonctionnement de l’appareil judiciaire et de l’administration publique… ICPEP a de la même manière mener plusieurs plaidoyers pour relever ces défis et plusieurs résultats se sont avérés satisfaisants ;</div>
                     </div>
                 </div>
 
                 <!--Service Block-->
                 <div class="service-block col-xl-3 col-lg-6 col-md-6 col-sm-12">
                 	<div class="inner-box">
-                    	<div class="icon-box"><span class="flaticon-medicine"></span></div>
+                    	<div class="icon-box" ><span class="flaticon-medicine"></span></div>
                         <h3>Medical Facilities</h3>
-                        <div class="text">Lorem ipsum dolor amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolor magna aliqua enim ad minimveniam quis nostrud exercitation laboris.</div>
+                        <div class="text" style="text-align: justify;">ICPEP a rendu public plusieurs rapports de monitoring des lieux de détention, adressés aux autorités judiciaires et exécutives compétentes pour l’amélioration de l’administration de la justice et l’assainissement du milieu carcéral (où loge une population oubliée) ;</div>
                     </div>
                 </div>
 
@@ -145,7 +145,7 @@
                 	<div class="inner-box">
                     	<div class="icon-box"><span class="flaticon-reading"></span></div>
                         <h3>Education</h3>
-                        <div class="text">Lorem ipsum dolor amet consectetur adipisicing elit sed eiusmod tempor incididunt labore dolor magna aliqua enim ad minimveniam quis nostrud exercitation laboris.</div>
+                        <div class="text" style="text-align: justify;">ICPEP a organisé plusieurs formations à l’intention de la population civile et autres bénéficiaires sur les droits humains et leurs mécanismes de protection, les mécanismes extra-judiciaires ou modes alternatifs de résolution des conflits, l’art culinaire (avec remise des brevets contresignés par le Ministre Provincial compétent). De façon particulière, une formation sur les VSBG et la protection de l’enfant à l’intention des membres de ICPEP,</div>
                     </div>
                 </div>
 
@@ -168,10 +168,14 @@
                 	<div class="text-column col-lg-6 col-md-12 col-sm-12">
                     	<div class="inner">
                             <div class="sec-title">
-                                <div class="sub-title">Proident sunt</div>
-                                <h2>Our Mission</h2>
-                                <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris Nisi ut aliquip commodo consequat.</div>
-                                <div class="link-box"><a href="cause-single.html" class="theme-btn btn-style-one"><span class="btn-title">Read More</span></a></div>
+                                <div class="sub-title">Ils sortent</div>
+                                <h2>Nos objectifs</h2>
+                                <div class="text" style="text-align: justify;"> à la transformation et conservation artisanale, semi-industrielle ou industrielle de production locale ;
+                                    • Participer dans les programmes de la conservation communautaire des forêts des communautés locales ;
+                                    • Participer dans des campagnes de promotion des droits humains, de l’environnement et de la paix ;
+                                    • Promouvoir la paix entre cultivateurs et éleveurs.
+                                    • Promouvoir et protéger les droits humains</div>
+                                <div class="link-box"><a href="{{ route('about')}}" class="theme-btn btn-style-one"><span class="btn-title">A savoir plus</span></a></div>
                             </div>
                         </div>
                     </div>
@@ -191,9 +195,13 @@
                 	<div class="text-column col-lg-6 col-md-12 col-sm-12">
                     	<div class="inner">
                             <div class="sec-title">
-                                <div class="sub-title">Proident sunt</div>
-                                <h2>Our Vision</h2>
-                                <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. enim ad minim veniam, quis nostrud exercitation ullamco laboris Nisi ut aliquip commodo consequat.</div>
+                                <div class="sub-title">Ils sortent</div>
+                                <h2>Nos Missions</h2>
+                                <div class="text" style="text-align: justify;">Mettre en place des projets communautaires d’auto-prise en charge (l’agriculture, l’élevage, usine, semi-industriel, alphabétisation, coupe et couture, art culinaire, tressage, coiffure, tissage, tricotage, cordonnerie, menuiserie, …) ;
+                                    Rendre opérationnel la commercialisation des produits agricoles et appuyer les membres des Groupes solidaires Paysans ;
+                                    Intensifier les plaidoyers pour la paix et la sécurité en vue de préparer le terrain aux actions de développement durable ;
+                                    Monitoring de protection pour lutter contre les violations des droits humains et les SVGB ;
+                                    Monitoring des lieux de détention ;</div>
                                 <div class="link-box"><a href="cause-single.html" class="theme-btn btn-style-one"><span class="btn-title">Read More</span></a></div>
                             </div>
                         </div>
