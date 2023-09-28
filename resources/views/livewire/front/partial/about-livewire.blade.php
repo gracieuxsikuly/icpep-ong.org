88f5e20dc86db6f3be0980024c47592920167862
gracieuxsikuly <graciersikuly@gmail.com>
2023-09-28 21:16:11 +0200

    good

--- resources/views/livewire/front/partial/about-livewire.blade.php
+++ resources/views/livewire/front/partial/about-livewire.blade.php
@@ -2,7 +2,7 @@
     <section class="about sec-padd2">
         <div class="container">
             <div class="section-title center">
-                <h2>About our <span class="thm-color">humanity</span></h2>
+                <h2>Apropos d'<span class="thm-color"> ICPEP</span></h2>
             </div>
 
 
@@ -16,10 +16,13 @@
 
 
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            <h3>What We Do</h3>
+                            <h3>Ce que nous faisons</h3>
 
 
-                            <p>Idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actually teachings of the great explorer of the truth.</p>
+                            <p style="text-align: justify;">
+                            Aspirer à la transformation et conservation artisanale, semi-industrielle ou industrielle de production locale ;<br>
+                            Participer dans les programmes de la conservation communautaire des forêts des communautés locales ;
+                            </p>
                         </div>
                     </div>
 
@@ -32,41 +35,41 @@
 
 
                         <div class="col-md-6 col-sm-6 col-xs-12">
-                            <h3>How It Works</h3>
+                            <h3>Nos Objectifs</h3>
 
 
-                            <p>Who loves or pursues or desires to obtain pain of itself, because it is pain, because occasionally circumstances occur which toil and pain can procure him some great pleasure.</p>
+                            <p style="text-align: justify;">Participer dans des campagnes de promotion des droits humains, de l’environnement et de la paix ;
+                                Promouvoir la paix entre cultivateurs et éleveurs ;
+                                Promouvoir et protéger les droits humains,
+                                </p>
                         </div>
                     </div>
                 </article>
 
 
                 <article class="col-md-6 col-sm-6 col-xs-12">
+                    <h3>Raison Sociale</h3>
                     <div class="content">
                         <div class="text">
-                            <p>When you give to Our humanity, you know your donation is making a difference. Whether you are supporting one of our Signature Programs or our carefully curated list of Gifts That Give More, our professional staff works hard every day to ensure every dollar has impact for the cause of your choice.</p>
+                            <p style="text-align: justify;">Lutter contre la violation des Droits Humains et des libertés fondamentales des citoyens, par la dénonciation et le plaidoyer ;
+                                Faciliter aux nécessiteux l’accès à la justice par l’accompagnement, la consultance, des conseils juridiques et l’assistance et la représentation judiciaires ;
+                                Organiser régulièrement des campagnes de sensibilisation sur l’importance de la protection et de la gestion rationnelle de l’environnement ;
+                                </p>
                         </div>
 
 
-                        <h3 class="thm-color">Years of Experience</h3>
+                        <h3 class="thm-color">Nos expériences</h3>
 
 
                         <div class="text">
-                            <p>We partner with over 320 amazing projects worldwide, and have given over $150 million in cash and product grants to other groups since 2011. We also operate our own dynamic suite of Signature Programs.</p>
+                            <p>ICPEP a une expérience dans la multiplication et productions des semences améliorées, adaptées et certifiées. De ce fait, il milite pour l’inscription des nouvelles variétés au catalogue variétal national conformément à la réglementation semencière en vigueur en RD Congo. </p>
                         </div>
 
 
                         <ul>
-                            <li><i class="fa fa-check"></i>This mistaken idea of denouncing pleasure</li>
-
-
-                            <li><i class="fa fa-check"></i>Master-builder of human happiness</li>
-
-
-                            <li><i class="fa fa-check"></i>Occasionally circumstances occur in toil</li>
-
-
-                            <li><i class="fa fa-check"></i>Undertakes laborious physical exercise</li>
+                            <li style="text-align: justify;"><i class="fa fa-check"></i>ICPEP promeut la protection de l’environnement, l’accompagnement juridique des agriculteurs en milieu rural et
+                                soutenir la promotion, le Monitoring et le suivi des cas de violation des droits humains et les SVGB. </li>
+                            <li style="text-align: justify;"><i class="fa fa-check"></i>Il offre également une expertise avérée en matière de résolution pacifique des conflits fonciers et coutumiers.</li>
                         </ul>
                     </div>
                 </article>
