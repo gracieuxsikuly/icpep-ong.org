ed63cdd826375dc5516245c1ecd63a9e8a3f5a82
gracieuxsikuly <graciersikuly@gmail.com>
2023-11-22 10:37:08 +0200

    super super

--- resources/views/layouts/admin/defaultlogin.blade.php
+++ resources/views/layouts/admin/defaultlogin.blade.php
@@ -21,6 +21,32 @@
         <!-- App Css-->
         <link href="{{asset ('aassets/css/app.min.css') }}" id="app-style" rel="stylesheet" type="text/css" />
 
+
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+        <!-- owl.carousel css -->
+        <link rel="stylesheet" href="assets/libs/owl.carousel/assets/owl.carousel.min.css">
+
+        <link rel="stylesheet" href="assets/libs/owl.carousel/assets/owl.theme.default.min.css">
+
+        <!-- Bootstrap Css -->
+        <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+
+
+
+
+
+
     </head>
 
     <body data-sidebar="dark">
