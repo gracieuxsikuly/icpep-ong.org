81df601ba16eaf22646deef18a0f8bffbaac1b5e
gracieuxsikuly <graciersikuly@gmail.com>
2023-11-23 16:48:29 +0200

    good

--- app/Http/Livewire/Admin/ListBlog.php
+++ app/Http/Livewire/Admin/ListBlog.php
@@ -3,11 +3,55 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use App\Models\Blog;
+use Livewire\WithPagination;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class ListBlog extends Component
 {
+    public $page_active=20,$desplayedelete,$searchTerm;
+    use WithPagination;
+    use LivewireAlert;
+    protected $listeners = [
+        'confirmed'
+    ];
+    public function delete($id)
+    {
+        $this->desplayedelete = $id;
+        $this->alert('warning', 'Etes vous sur de vouloir suprimer?', [
+            'showConfirmButton' => true,
+            'confirmButtonText' => 'Suprimer',
+            'showCancelButton' => true,
+            'cancelButtonText' => 'Cancel',
+            'onConfirmed' => 'confirmed',
+            'onDismissed' => 'cancelled',
+            'position' => 'center'
+        ]);
+    }
+    public function confirmed()
+    {
+
+        $methodel = Blog::whereId($this->desplayedelete)->delete();
+        if ($methodel) {
+            $this->alert('info', 'projet bien Suprime!');
+        }
+    }
     public function render()
     {
-        return view('livewire.admin.list-blog');
+        if ($this->searchTerm) {
+            $searchTerm = '%' . $this->searchTerm . '%';
+
+            $blogs = Blog::where('titre', 'LIKE', $searchTerm)
+                ->orWhere('contenu', 'LIKE', $searchTerm)
+                ->orWhere('publie', 'LIKE', $searchTerm)
+                ->orWhere('notation', 'LIKE', $searchTerm)
+                ->orWhere('image', 'LIKE', $searchTerm)
+                ->orWhere('vue', 'LIKE', $searchTerm)
+                ->paginate($this->page_active);
+        } else {
+            $blogs = Blog::paginate($this->page_active);
+        }
+
+        return view('livewire.admin.list-blog', ['blogs' => $blogs]);
     }
 }
--- resources/views/livewire/admin/list-blog.blade.php
+++ resources/views/livewire/admin/list-blog.blade.php
@@ -1,3 +1,73 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="row">
+        <div class="col-12">
+            <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <!-- Barre de recherche -->
+                        <div class="input-group mb-2">
+                            <input wire:model="searchTerm" type="text" class="form-control" placeholder="Rechercher une méthode de livraison">
+                            <button type="submit" class="btn btn-primary"><i class="mdi mdi-magnify"></i> </button>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <!-- Bouton "Nouvelle méthode de livraison" -->
+                        <a href="{{ route('ajoutblog') }}" class="btn btn-primary mb-2"><i class="mdi mdi-plus-circle me-2"></i> Nouvelle publication</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Table Edit</h4>
+                    <div class="table-responsive">
+                        <table class="table table-editable table-nowrap align-middle table-edits">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Image</th>
+                                    <th>Titre</th>
+                                    <th>Contenu</th>
+                                    <th>Auteur</th>
+                                    <th>Publié</th>
+                                    <th>Notation</th>
+
+                                    <th>Vues</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($blogs as $blog)
+                                    <tr data-id="{{ $blog->id }}">
+                                        <td data-field="id" style="width: 80px">{{ $blog->id }}</td>
+                                        <td data-field="image">
+                                            <img src="{{ asset('assets/images/publication/' . $blog->image) }}" alt="{{ $blog->image }}" class="avatar-sm">
+                                            </td>
+                                        <td data-field="titre">{{ $blog->titre }}</td>
+                                        <td data-field="contenu">{{ Str::of($blog->contenu)->limit(15) }}</td>
+                                        <td data-field="auteur">{{ $blog->auteur->name }}</td>
+                                        <td data-field="publie">{{ $blog->publie }}</td>
+                                        <td data-field="notation">{{ $blog->notation }}</td>
+
+                                        <td data-field="vue">{{ $blog->vue }}</td>
+                                        <td style="width: 100px">
+                                            <a wire:click.prevent="delete({{$blog->id}})"  class="btn btn-outline-secondary btn-sm edit" title="Delete">
+                                                <i class="mdi mdi-delete"></i>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+
+                </div>
+            </div>
+        </div> <!-- end col -->
+    </div>
 </div>
