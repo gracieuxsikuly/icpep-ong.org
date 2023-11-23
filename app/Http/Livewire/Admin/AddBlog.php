07401e1a6630279ffe3c259194b8f25f3c960712
gracieuxsikuly <graciersikuly@gmail.com>
2023-11-23 11:55:24 +0200

    okays

--- app/Http/Livewire/Admin/AddBlog.php
+++ app/Http/Livewire/Admin/AddBlog.php
@@ -2,10 +2,67 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Blog;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+use Intervention\Image\Facades\Image;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class AddBlog extends Component
 {
+    use WithFileUploads;
+    use LivewireAlert;
+    public $titre;
+    public $contenu;
+    // public $auteur_id;
+    // public $slug;
+    public $publie;
+    // public $notation;
+    public $image;
+    // public $vue;
+    protected $rules = [
+        'titre' => 'required|string',
+        'contenu' => 'required|string',
+        'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+    ];
+    protected $messages = [
+        'titre.required' => 'Le champ Titre est obligatoire.',
+        'titre.string' => 'Le champ Titre doit être une chaîne de caractères.',
+
+        'contenu.required' => 'Le champ Contenu est obligatoire.',
+        'contenu.string' => 'Le champ Contenu doit être une chaîne de caractères.',
+
+        'image.required' => 'Le champ Image est obligatoire.',
+        'image.image' => 'Le champ Image doit être une image.',
+        'image.mimes' => 'Le champ Image doit être une image de type jpeg, png, jpg ou gif.',
+        'image.max' => 'Le champ Image ne doit pas dépasser :max kilo-octets.',
+    ];
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+    public function submit()
+    {
+        $this->validate();
+        if ($this->image) {
+            $imageName = $this->image->getClientOriginalName();
+                $imageName = time() . $imageName;
+                $imageResized = Image::make($this->image->getRealPath())
+                ->save(public_path('assets/images/publication/' . $imageName));
+        }
+        $blog = new Blog();
+        $blog->titre = $this->titre;
+        $blog->contenu = $this->contenu;
+        $blog->auteur_id = auth()->user()->id;
+        $blog->slug = $this->titre;
+        $blog->publie = $this->publie;
+        $blog->notation = 0;
+        $blog->image = $this->image ? $imageName : null;
+        $blog->vue = 0;
+        $blog->save();
+        $this->alert('success', 'blog bien creer!');
+        // return redirect()->route('admin.blogs');
+    }
     public function render()
     {
         return view('livewire.admin.add-blog');
--- resources/views/layouts/admin/pages/ajoutblog.blade.php
+++ resources/views/layouts/admin/pages/ajoutblog.blade.php
@@ -21,31 +21,50 @@
             <div class="card-body">
                 <h4 class="card-title mb-4">Ajouter la publication</h4>
 
-                <form>
+                <form wire:submit.prevent="submitForm" enctype="multipart/form-data">
                     <div class="mb-3">
-                        <label for="formrow-firstname-input" class="form-label">First name</label>
-                        <input type="text" class="form-control" id="formrow-firstname-input">
+                        <label for="titre" class="form-label">Titre</label>
+                        <input wire:model="titre" type="text" class="form-control" id="titre">
+                        @error('titre') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
+
                     <div class="mb-3">
-                        <label for="formrow-firstname-input" class="form-label">First name</label>
-                        <input type="text" class="form-control" id="formrow-firstname-input">
-                    </div>
-                    <div class="mb-3">
-                        <label for="formrow-firstname-input" class="form-label">First name</label>
-                        <input type="text" class="form-control" id="formrow-firstname-input">
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Textarea</label>
-                        <div>
-                            <textarea required class="form-control" rows="3"></textarea>
-                        </div>
+                        <label for="contenu" class="form-label">Contenu</label>
+                        <textarea wire:model="contenu" class="form-control" rows="3"></textarea>
+                        @error('contenu') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
 
 
 
+                    {{-- <div class="mb-3">
+                        <label for="publie" class="form-label">Publié</label>
+                        <select wire:model="publie" class="form-control" id="publie">
+                            <option value="1">Oui</option>
+                            <option value="0">Non</option>
+                        </select>
+                        @error('publie') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div> --}}
+
+                    {{-- <div class="mb-3">
+                        <label for="notation" class="form-label">Notation</label>
+                        <input wire:model="notation" type="text" class="form-control" id="notation">
+                        @error('notation') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div> --}}
+
+                    <div class="mb-3">
+                        <label for="image" class="form-label">Image</label>
+                        <input wire:model="image" type="file" class="form-control" id="image">
+                        @error('image') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div>
+
+                    {{-- <div class="mb-3">
+                        <label for="vue" class="form-label">Vue</label>
+                        <input wire:model="vue" type="text" class="form-control" id="vue">
+                        @error('vue') <span class="text-danger">{{ $message }}</span> @enderror
+                    </div> --}}
 
                     <div>
-                        <button type="submit" class="btn btn-primary w-md">Submit</button>
+                        <button type="submit" class="btn btn-primary w-md">Enregistrer la publication</button>
                     </div>
                 </form>
             </div>
