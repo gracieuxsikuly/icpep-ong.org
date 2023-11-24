a68549ec94840389d3cce6c3eeb4fbb681655887
gracieuxsikuly <graciersikuly@gmail.com>
2023-11-24 12:37:36 +0200

    dff

--- app/Http/Livewire/Front/Partial/EventLivewire.php
+++ app/Http/Livewire/Front/Partial/EventLivewire.php
@@ -3,11 +3,20 @@
 namespace App\Http\Livewire\Front\Partial;
 
 use Livewire\Component;
+use App\Models\Blog;
+use Livewire\WithPagination;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class EventLivewire extends Component
 {
+    public $page_active=3;
+    use WithPagination;
+    use LivewireAlert;
     public function render()
     {
-        return view('livewire.front.partial.event-livewire');
+         // chargement de 3 derniers projet
+         $blogs = Blog::orderBy('id', 'desc')->take(3)->get();
+         return view('livewire.front.partial.event-livewire',
+         ['blogs' => $blogs]);
     }
 }
--- resources/views/livewire/front/partial/event-livewire.blade.php
+++ resources/views/livewire/front/partial/event-livewire.blade.php
@@ -7,10 +7,13 @@
 
 
             <div class="row">
+                @forelse ($blogs as $blog)
                 <article class="col-md-4 col-sm-6 col-xs-12">
                     <div class="default-blog-news">
                         <figure class="img-holder">
-                            <a href="blog-details.html"><img alt="News" src="{{asset('assets/images/blog/1.jpg')}}"></a>
+                            <a href="blog-details.html">
+                                <img src="{{ asset('assets/images/blog/' . $blog->image) }}" alt="{{ $blog->image }}">
+                                {{-- <img alt="News" src="{{asset('assets/images/blog/1.jpg')}}"></a> --}}
 
                             <div class="inner-box">
                             </div>
@@ -20,23 +23,18 @@
                         <div class="overlay">
                             <div class="bottom-box">
                                 <div class="category">
-                                    Conscience
+                                    {{ Str::of( $blog->titre)->limit(45) }}
                                 </div>
 
                                 <div class="content">
                                     <div class="post-meta">
-                                        Janvier 05, 2020 | 10 Comments
+                                        {{ $blog->created_at }} |  {{ $blog->vue }} Vues
                                     </div>
                                     <a href="blog-details.html">
-                                    <h4>ICPEP : Rapports sur la Protection des Civils
-                                         Dénonciation des Violations des Droits Humains</h4></a>
+                                    <h4> {{ $blog->titre }}</h4></a>
 
                                     <div class="text">
-                                        <p>{{ Str::of('ICPEP a élaboré plusieurs rapports sur la protection des civils, dénonciations des violations<br>
-                                            des droits humains-spoliation des biens publics de l’Etat-détournements des deniers <br>
-                                            publics-dysfonctionnement de l’appareil judiciaire et de l’administration publique… ICPEP
-                                            a de la même manière mener plusieurs plaidoyers pour relever ces défis et plusieurs
-                                            résultats se sont avérés satisfaisants ;')->limit(200) }}</p>
+                                        <p>{{ Str::of( $blog->contenu)->limit(200) }}</p>
                                     </div>
                                 </div>
                             </div>
@@ -45,125 +43,24 @@
 
                         <div class="lower-content">
                             <div class="category">
-                                Awarness
+                                {{ Str::of( $blog->titre)->limit(45) }}
                             </div>
 
 
                             <div class="content">
                                    <div class="post-meta">
-                                    Janvier 05, 2020 | 10 Comments
+                                    {{ $blog->created_at }} |  {{ $blog->notation }} Etoiles
                                 </div>
                                 <a href="blog-details.html">
-                                    <h4>ICPEP : Rapports sur la Protection des Civils</h4></a>
+                                    <h4> {{ Str::of( $blog->titre)->limit(45) }}</h4></a>
                             </div>
                         </div>
                     </div>
                 </article>
-                <article class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="default-blog-news">
-                        <figure class="img-holder">
-                            <a href="blog-details.html"><img alt="News" src="{{asset('assets/images/blog/1.jpg')}}"></a>
+                @empty
+                <p>Aucune publication disponible</p>
 
-                            <div class="inner-box">
-                            </div>
-                        </figure>
-
-
-                        <div class="overlay">
-                            <div class="bottom-box">
-                                <div class="category">
-                                    Conscience
-                                </div>
-
-                                <div class="content">
-                                    <div class="post-meta">
-                                        Janvier 05, 2020 | 10 Comments
-                                    </div>
-                                    <a href="blog-details.html">
-                                    <h4>ICPEP : Rapports sur la Protection des Civils
-                                         Dénonciation des Violations des Droits Humains</h4></a>
-
-                                    <div class="text">
-                                        <p>{{ Str::of('ICPEP a élaboré plusieurs rapports sur la protection des civils, dénonciations des violations<br>
-                                            des droits humains-spoliation des biens publics de l’Etat-détournements des deniers <br>
-                                            publics-dysfonctionnement de l’appareil judiciaire et de l’administration publique… ICPEP
-                                            a de la même manière mener plusieurs plaidoyers pour relever ces défis et plusieurs
-                                            résultats se sont avérés satisfaisants ;')->limit(200) }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="lower-content">
-                            <div class="category">
-                                Awarness
-                            </div>
-
-
-                            <div class="content">
-                                   <div class="post-meta">
-                                    Janvier 05, 2020 | 10 Comments
-                                </div>
-                                <a href="blog-details.html">
-                                    <h4>ICPEP : Rapports sur la Protection des Civils</h4></a>
-                            </div>
-                        </div>
-                    </div>
-                </article>
-
-                <article class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="default-blog-news">
-                        <figure class="img-holder">
-                            <a href="blog-details.html"><img alt="News" src="{{asset('assets/images/blog/1.jpg')}}"></a>
-
-                            <div class="inner-box">
-                            </div>
-                        </figure>
-
-
-                        <div class="overlay">
-                            <div class="bottom-box">
-                                <div class="category">
-                                    Conscience
-                                </div>
-
-                                <div class="content">
-                                    <div class="post-meta">
-                                        Janvier 05, 2020 | 10 Comments
-                                    </div>
-                                    <a href="blog-details.html">
-                                    <h4>ICPEP : Rapports sur la Protection des Civils
-                                         Dénonciation des Violations des Droits Humains</h4></a>
-
-                                    <div class="text">
-                                        <p>{{ Str::of('ICPEP a élaboré plusieurs rapports sur la protection des civils, dénonciations des violations<br>
-                                            des droits humains-spoliation des biens publics de l’Etat-détournements des deniers <br>
-                                            publics-dysfonctionnement de l’appareil judiciaire et de l’administration publique… ICPEP
-                                            a de la même manière mener plusieurs plaidoyers pour relever ces défis et plusieurs
-                                            résultats se sont avérés satisfaisants ;')->limit(200) }}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <div class="lower-content">
-                            <div class="category">
-                                Awarness
-                            </div>
-
-
-                            <div class="content">
-                                   <div class="post-meta">
-                                    Janvier 05, 2020 | 10 Comments
-                                </div>
-                                <a href="blog-details.html">
-                                    <h4>ICPEP : Rapports sur la Protection des Civils</h4></a>
-                            </div>
-                        </div>
-                    </div>
-                </article>
+                @endforelse
             </div>
         </div>
     </section>
