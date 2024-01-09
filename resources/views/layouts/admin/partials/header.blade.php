d23b7e9155424b2c6760ce50825e2bca4b294f53
gracieuxsikuly <graciersikuly@gmail.com>
2024-01-09 13:42:34 +0200

    good

--- resources/views/layouts/admin/partials/header.blade.php
+++ resources/views/layouts/admin/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a href="{{ route('/') }}" class="logo logo-dark">
+                <a href="{{ route('home') }}" class="logo logo-dark">
                     <span class="logo-sm">
                         <img src="{{asset('assets/images/logo/logo.png')}}" alt="" height="22">
                     </span>
@@ -12,7 +12,7 @@
                     </span>
                 </a>
 
-                <a href="{{ route('/') }}" class="logo logo-light">
+                <a href="{{ route('home') }}" class="logo logo-light">
                     <span class="logo-sm">
                         <img src="{{asset('assets/images/logo/logo.png')}}" alt="" height="22">
                     </span>
