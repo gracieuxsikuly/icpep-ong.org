880e6a1ae2c3380d76e3d0da3ccb9a6dddcecb19
gracieuxsikuly <graciersikuly@gmail.com>
2024-01-09 15:48:17 +0200

    god

--- resources/views/layouts/admin/partials/header.blade.php
+++ resources/views/layouts/admin/partials/header.blade.php
@@ -25,10 +25,6 @@
             <button type="button" class="btn btn-sm px-3 font-size-16 header-item waves-effect" id="vertical-menu-btn">
                 <i class="fa fa-fw fa-bars"></i>
             </button>
-
-
-
-
         </div>
 
         <div class="d-flex">
