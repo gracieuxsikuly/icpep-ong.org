6f961f58e57076199fc381ed4abd5043597d65f4
gracieuxsikuly <graciersikuly@gmail.com>
2024-06-05 11:10:57 +0200

    dgdg

--- resources/views/layouts/partials/partenaire.blade.php
+++ resources/views/layouts/partials/partenaire.blade.php
@@ -1,19 +1,19 @@
 <section class="clients-section">
     <div class="container">
         <div class="client-carousel owl-carousel owl-theme">
-            <div class="item tool_tip" title="media partner"><img alt="Awesome Image" src="{{asset('assets/images/clients/1.jpg')}}">
+            <div class="item tool_tip" title="CADEP"><img alt="Awesome Image" src="{{asset('assets/images/clients/1.jpg')}}">
             </div>
 
 
-            <div class="item tool_tip" title="media partner"><img alt="Awesome Image" src="{{asset('assets/images/clients/2.jpg')}}">
+            <div class="item tool_tip" title="IMA"><img alt="Awesome Image" src="{{asset('assets/images/clients/2.jpg')}}">
             </div>
 
 
-            <div class="item tool_tip" title="media partner"><img alt="Awesome Image" src="{{('assets/images/clients/3.webp')}}">
+            <div class="item tool_tip" title="UCNDK"><img alt="Awesome Image" src="{{('assets/images/clients/3.webp')}}">
             </div>
 
 
-            <div class="item tool_tip" title="media partner"><img alt="Awesome Image" src="{{asset('assets/images/clients/virunga.webp')}}">
+            <div class="item tool_tip" title="VIRUNGA FONDATION"><img alt="Awesome Image" src="{{asset('assets/images/clients/virunga.webp')}}">
             </div>
 
         </div>
