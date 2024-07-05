57a5c974a134ceb44b0571fbfacd69ef9fa1fca5
gracieuxsikuly <graciersikuly@gmail.com>
2024-07-05 11:33:58 +0200

    good

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -9,7 +9,7 @@ class Blog extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'titre', 'contenu', 'auteur_id', 'slug', 'publie', 'notation','image','vue'
+        'titre', 'contenu', 'auteur_id', 'slug', 'publie', 'notation','image','vue','videolink'
     ];
 
     public function auteur()
